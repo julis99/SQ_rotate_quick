aa37ffdccc86ae7934197ee5cc6de8c360cba388
julis_99 <thejulis99@gmail.com>
2025-01-31 18:58:31 +0100

    added Couple and Square classes

---
+++ Couple.py
@@ -0,0 +1,20 @@
+from Dancer import *
+
+class Couple:
+    def __init__(self, boy: Dancer, girl: Dancer):
+        self.boy = boy
+        self.girl = girl
+    
+
+    def __str__(self):
+        rtn = ""
+        rtn += f"{self.boy:<15} | {self.girl:<15}"
+        return rtn
+
+
+
+
+if __name__ == "__main__":
+    dn1 = loadDancer(370452)
+    print(Couple(dn1, dn1))
+        
