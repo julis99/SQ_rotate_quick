b1603747adbf57e73ae3caf94575322121c5b404
julis_99 <thejulis99@gmail.com>
2025-05-15 12:34:03 +0200

    Update test.py to feature better testing function

--- tests.py
+++ tests.py
@@ -21,24 +21,44 @@ def EVAL_dancers_danced(prints: bool = False) -> {int, int}:
     lst = load_data_base()
     mini = Fraction(1, 1)
     maxi = Fraction(0, 1)
+    zero = Fraction(0, 0)
     for dnc in lst:
+        if dnc.getDancedFactor() == zero:
+            continue
         if dnc.getDancedFactor() < mini:
             mini = dnc.getDancedFactor()
         if dnc.getDancedFactor() > maxi:
             maxi = dnc.getDancedFactor()
     if prints:
-        lst = sort_list_by_num_danced(lst)
-        print(r"##############################EVAL##############################")
+        lst = sort_list_by_last_danced(lst)
+        print(r"###############################EVAL###############################")
         print(r"                      Dancer Participation")
         print(r"##################################################################")
         print(f" Minimum of particpations: {mini}")
         print(f" Maximum of particpations: {maxi}")
+        print(f" Difference in NUM       : {maxi.numerator - mini.numerator}")
         print(r"##################################################################")
         print(r" Dancers:")
+        lst = sort_list_by_danced_factor(lst)
         for dnc in lst:
+            if dnc.getDancedFactor() == zero:
+                continue
             print(f"    {dnc:+n}")
+        print(r"##############################ENDEVAL#############################")
     return [mini, maxi]
 
+def TEST_test_rotation(runs: int):
+    rt = Rotate()
+    rt.reloadLists()
+    rt.resetDancers()
+    print(r"###############################TEST###############################")
+    print(r"##################################################################")
+    print(f" Performing {runs} rounds with {rt.possibleSquares} possible squares each")
+    for i in range(runs):
+        rt.newRound()
+    EVAL_dancers_danced(True)
+    print(r"##################################################################")
+    print(r"##############################ENDTEST#############################")
 
 if __name__ == "__main__":
-    EVAL_dancers_danced(True)
+    TEST_test_rotation(100)
